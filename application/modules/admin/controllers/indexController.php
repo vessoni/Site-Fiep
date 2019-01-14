5e2a5b14a12b61e943a0b6d389f9533f8f4ba4b0
Alexandre Vessoni <av.vessoni@gmail.com>
2019-01-14 10:49:49 -0300

    Ajustados Editar com novos campos de subcategoria

--- application/modules/admin/controllers/indexController.php
+++ application/modules/admin/controllers/indexController.php
@@ -49,16 +49,36 @@ class Admin_indexController extends App_Controller_Action
 
         if ($this->_request->isPost()) {
             $data = $this->_request->getPost();
+            $caminho = $this->upload($_FILES['arquivo']['name']);
+
                 $subcategoria = new Application_Model_Atena_TipoMdl();
                 $dadosProcessados = array(
                     "idtipo" => $this->_request->getParam("id"),
                     "nome" =>  $data['nome'],
                     "descricao" => $data['descricao'],
+                    "resumo" => $data['resumo'],
+                    "img_destaque" => $caminho,
+                    "icon" => $data['icon']
                 );
                 $subcategoria->_update($dadosProcessados);
                 $this->_redirect('/admin/subcategoria/'.$this->_request->getParam("id"));
         }
     }
+
+    public function upload(){
+
+      $pieces = explode('\\', dirname(__FILE__));
+      $path = $pieces[0].'\\'.$pieces[1].'\\'.$pieces[2].'\\public\\static\\upload\\images\\';
+      $name = microtime()."-".$_FILES['arquivo']['name'];
+
+      $destino = $path.$name;
+      $arquivo_tmp = $_FILES['arquivo']['tmp_name'];
+
+      move_uploaded_file( $arquivo_tmp, $destino );
+
+      return $name;
+
+  }
 
     public function editAction(){
         $atividades = new Application_Model_Atena_CategoriaMdl();
